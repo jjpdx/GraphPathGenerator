a9a9387112bc571413f398437d7ab457dbd0840c
John Jones <jjpdxconsulting@gmail.com>
2021-05-30 18:47:29 -0700

    - Initial Checkin

--- Core/Services/PathfinderService.cs
+++ Core/Services/PathfinderService.cs
@@ -8,43 +8,68 @@ namespace GraphPathGenerator.Core.Services
 {
     public class PathfinderService
     {
-        //Creates a dictionary of each vertex with their Id as the key, the value of which is a tuple of their direct ancestor's Id and the overall distance from the start vertex.
-        public Dictionary<Guid, Tuple<Guid, int>> GetProximityIndex(Guid StartVertex, List<Vertex> vertices, List<Edge> edges)
+        public LinkedList<Guid> GetShortestPath (Guid startVertex, Guid endVertex, List<Vertex> vertices, List<Edge> edges)
         {
-            var result = new Dictionary<Guid, int>();
-
-            result.Add(StartVertex, 0); // add starting vertex with distance 0
-            var currentVertex = StartVertex;
-            var distanceIndex = 1; //
-
-            var currentDistanceQueue = new Queue<Guid>();
-            currentDistanceQueue.Enqueue(StartVertex);
-
-            while (result.Count < vertices.Count) //ends when all vertices are accounted for, currently assumes that all vertices are connected.
+            var proximityMap = GetProximityMap(startVertex, vertices, edges);
+            var endMapNode = proximityMap.FirstOrDefault(n => n.Key == endVertex); //get end vertex from map
+            var path = new LinkedList<Guid>();
+            var currentNode = endMapNode;
+            for (int i = endMapNode.Value.Item2; i >= 0 ; i--) //start at distance of endvertex, continue until distance is zero at startvertex
             {
-                while(currentDistanceQueue.Count > 0) 
-                    foreach (Guid g in FindAllAdjacentVertices(currentDistanceQueue.Dequeue(), vertices, edges))
-                    {
-                        if (!result.ContainsKey(g))
-                        {
-                            result.Add(g, distanceIndex);
-                        }
-                    }
-
-
-                distanceIndex += 1;
+                path.AddFirst(currentNode.Key);
+                currentNode = proximityMap.FirstOrDefault(n => n.Key == currentNode.Value.Item1);; //set current node key to direct ancestor
             }
 
+            return path;
+        }
+
+        //Creates a dictionary of each vertex with their Id as the key, the value of which is a tuple of their direct ancestor's Id and the overall distance from the start vertex.
+        public Dictionary<Guid, Tuple<Guid, int>> GetProximityMap(Guid startVertex, List<Vertex> vertices, List<Edge> edges)
+        {
+            var result = new Dictionary<Guid, Tuple<Guid, int>>();
+
+            result.Add(startVertex, Tuple.Create(Guid.Empty, 0)); // add starting vertex with distance 0
+            var distanceIndex = 1; //index of all adjacent new vertices in map.
+
+            var currentDistanceQueue = new Queue<Guid>(); //currently iterating vertices at distanceIndex from startvertex
+            currentDistanceQueue.Enqueue(startVertex);
+
+            var nextDistanceQueue = new List<Guid>(); //temp list to hold all vertices currently being identified at next increment of distance
+
+            while (result.Count < vertices.Count) //ends when all vertices are accounted for or when there are no more connected vertices to explore
+            {
+                while (currentDistanceQueue.Count > 0) //while queue is not empty
+                {
+                    var currentVertex = currentDistanceQueue.Dequeue();
+                    foreach (Guid g in FindAllAdjacentVertices(currentVertex, vertices, edges))
+                    {
+                        if (!result.ContainsKey(g)) //if key has not already been explored (any vertex that has already been added must have a shorter or equivalent length path from start vertex)
+                        {
+                            result.Add(g, Tuple.Create(currentVertex, distanceIndex));
+                            nextDistanceQueue.Add(g);
+                        }
+                    }
+                }
+
+                distanceIndex += 1;
+                foreach (Guid g in nextDistanceQueue)
+                    currentDistanceQueue.Enqueue(g);
+
+                if (nextDistanceQueue.Count == 0) //if there are no more adjacent vertices that haven't already been explored then end the loop.
+                    break;
+
+                nextDistanceQueue.Clear();
+            }
 
             return result;
         }
 
-        public List<Guid> FindAllAdjacentVertices(Guid VertexId, List<Vertex> vertices, List<Edge> edges)
+        public List<Guid> FindAllAdjacentVertices(Guid vertexId, List<Vertex> vertices, List<Edge> edges)
         {
             var result = new List<Guid>();
-            foreach(Edge e in edges.Where(e => e.ContainsVertex(VertexId)))
+            foreach(Edge e in edges.Where(e => e.ContainsVertex(vertexId)))
             {
-                result.Add(e.GetMatchingVertexPartner(VertexId));
+                result.Add(e.GetMatchingVertexPartner(vertexId));
             }
             return result;
         }
